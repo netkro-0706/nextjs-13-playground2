bf0ea23e59f16cf7dd7583483adfcc9fc6a1ca23
netkro <kroging@gmail.com>
2023-12-11 16:54:37 +0900

    fix: border追加、形を整える

--- src/app/components/primitives/carousel/style.css.ts
+++ src/app/components/primitives/carousel/style.css.ts
@@ -1,20 +1,36 @@
 import { style } from '@vanilla-extract/css';
 
-export const CarouselPackage = style({
-  display: 'flex',
-  flexDirection: 'column',
-  justifyContent: 'center',
-  alignItems: 'center',
-  width: '80px',
-  height: '80px',
-  border: '4px solid yellow',
-  padding: 10,
+export const PackageWrapper = style({
+  width: 320 + 13,
+  height: 180 + 53,
+  margin: '4px',
   transition: 'all 0.5s ease',
   ':hover': {
     transform: 'scale(1.2)',
   },
 });
 
-export const CarouselWrapper = style({
-  marginTop: '20px',
+export const OuterBorder = style({
+  border: '4px solid yellow',
+  borderRadius: '20px',
+  backgroundColor: 'black',
+});
+
+export const InnerBorder = style({
+  border: '4px solid black',
+  borderRadius: '14px',
+});
+
+export const ItemContainer = style({
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  alignItems: 'center',
+  position: 'relative',
+  overflow: 'hidden',
+  borderRadius: '10px',
+});
+
+export const DetailContainer = style({
+  backgroundColor: 'black',
 });
